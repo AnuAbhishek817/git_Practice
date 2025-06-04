24c9d3458f13162e2a86d2439fdbdbbcd1514828
Abhishek <abhishek.127105@gmail.com>
2025-06-05 03:26:45 +0530

    Added sum py file

---
+++ findsum.py
@@ -0,0 +1,5 @@
+firstnum = int(input("Enter first number: "))
+secondnum = int(input("Enter second number: "))
+result = firstnum + secondnum
+print("The sum of", firstnum, "and", secondnum, "is:", result)
+
--- firstproject.py
+++ firstproject.py
@@ -1,2 +1,3 @@
 print("Hello Abhishek")
 print("This is my first project in GitHub")
+print("I n adding new files in GitHub")
