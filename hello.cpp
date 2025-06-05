c8e556128a2a48e4f3f21a07eaadfda4cf2eee51
Abhishek <abhishek.127105@gmail.com>
2025-06-05 12:40:55 +0530

    two files added named findsum.py and hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cstdlib>   // For memory allocation functions      
+using namespace std;
+// namespace namestd;
+
+
+int main() {
+    cout<<"Hello, World!" << endl;
+    // Declare a pointer to an integer
+}
